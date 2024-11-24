aac5a1b268da51a5d7e4e5a91aaf9d18dca80056
Sancal0606 <carloz1.2.sanchez@gmail.com>
2024-11-24 13:07:57 +0100

    f: builtin commands

--- closh.c
+++ closh.c
@@ -6,9 +6,65 @@
  *
  * Modified by Ilker Demirkol
  */
+
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 #define MAX_LINE 80 /* 80 chars per line, per command */
+
+void cd()
+{
+    char directory[30];
+    scanf("%s", &directory);
+    if (strcmp(directory, " ") == 0)
+    {
+        chdir("/home");
+    }
+    else
+    {
+        chdir(directory);
+    }
+    printf("vvvvv\n");
+}
+
+void md()
+{
+    char directory[30];
+    scanf("%s", &directory);
+    mkdir(directory, 0700);
+}
+
+void rd()
+{
+    char directory[30];
+    scanf("%s", &directory);
+    rmdir(directory);
+}
+
+void builtIn()
+{
+    char cmd[30];
+    scanf("%s", cmd);
+    if (strcmp(cmd, "cd") == 0)
+    {
+        printf("%s is a shell builtin \n", cmd);
+    }
+
+    else if (strcmp(cmd, "md") == 0)
+    {
+        printf("%s is a shell builtin \n", cmd);
+    }
+
+    else if (strcmp(cmd, "rd") == 0)
+    {
+        printf("%s is a shell builtin \n", cmd);
+    }
+
+    else
+    {
+        printf("%s is external \n", cmd);
+    }
+}
 
 int main(void)
 {
@@ -22,10 +78,32 @@ int main(void)
         printf(directory);
         printf(">");
         // get command
-        char command[30];
+        char *command[30];
         scanf("%s", &command);
-        printf("Hello %s", command);
-        fflush(stdout);
+        if (strcmp(command, "cd") == 0)
+        {
+            cd();
+        }
+
+        if (strcmp(command, "md") == 0)
+        {
+            md();
+        }
+
+        if (strcmp(command, "rd") == 0)
+        {
+            rd();
+        }
+
+        if (strcmp(command, "builtin") == 0)
+        {
+            builtIn();
+        }
+
+        if (strcmp(command, "q") == 0)
+        {
+            return 0;
+        }
     }
     return 0;
 }
